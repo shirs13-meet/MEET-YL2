50c42211e7dd1198e239ee60908b23eb62c221c7
Shir Somech <shirs13@meet.mit.edu>
2014-11-17 18:35:13 +0200

    Animal Kingdom

---
+++ Lab2/OOP.py
@@ -0,0 +1,33 @@
+class Animal:
+	def __init__(self, name, age):
+		self.name=name
+		self.age=age
+	
+	def sleep(self):
+		print self.name + "is sleeping"
+
+	def eat(self):
+		print self.name + "is eating"
+
+class Kangaroo(Animal):
+	
+	def __init__(self, joeyName)
+		Animal__init__(name, age)
+		self.joeyName = joeyName
+	
+	def jumping (self, jumps):
+		print self.name + "jumped" + jumps + "times"
+
+	def child(self):
+		print self.name + "'s joey is named" + self.joeyName
+
+class Dog(Animal):
+
+	def __init__(self, breed, color):
+		Animal__init__(name, age)
+		self.breed=breed
+		self.color=color
+
+	def bark (self):
+		print self.name + "is barking"
+	
